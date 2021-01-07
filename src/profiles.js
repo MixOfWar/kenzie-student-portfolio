29cd191636623850960e304299d6f8890b55809e
Mia Peace <mrssilentwar@gmail.com>
2021-01-07 09:23:23 -0800

    added assets folder, need user pictures

---
+++ src/profiles.js
@@ -0,0 +1,10 @@
+const Profiles = [
+	{
+		id: 9181,
+		name: 'Mia Peace',
+		image: require('./assets/miapeace.jpg'),
+		role: 'Front End Developer',
+		about:
+			'Cultivating a passion for technology from a young age I have pushed myself to focus on the visual format as well as the accessibility of technology. Moving forward within this world requires that we carefully tend to the way content is presented to reach many and encourage the development of a table that not only includes all but bolsters diversity. Through this strategy, we enrich the environment around us and nourish new innovative ideas to move technology forward. Beyond my love for technology, I am a disability advocate, an outdoor enthusiast, a reader, a writer, a dancer, and a singing lover of life. I know that by keeping our lives rounded and our mind entrenched in learning we can move forward in a positive way, bringing out not only the best in ourselves but in those around us. My goals are to be a decisive designer and a valuable team member always striving for not only my best but the best from my team. I enjoy working with others and love when design can be refined within an attainable scope.',
+	},
+];
