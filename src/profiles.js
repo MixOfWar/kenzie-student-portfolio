df6b67dd89996707ae9583efd48717a09fb77a86
Mia Peace <mrssilentwar@gmail.com>
2021-01-07 10:14:15 -0800

    Data structure done for profiles

--- src/profiles.js
+++ src/profiles.js
@@ -1,4 +1,23 @@
-const Profiles = [
+const userProfiles = [
+	{
+		id: 7426,
+		name: 'Fredo Sieck',
+		image: require('./assets/fms-avatar.jpeg'),
+		role: '',
+		about: '',
+		highlights: '',
+		skills: [],
+		education: [],
+		projects: [
+			{
+				type: 0,
+				title: '',
+				text: '',
+				images: false,
+			},
+		],
+		contact: '',
+	},
 	{
 		id: 9181,
 		name: 'Mia Peace',
@@ -6,5 +25,130 @@ const Profiles = [
 		role: 'Front End Developer',
 		about:
 			'Cultivating a passion for technology from a young age I have pushed myself to focus on the visual format as well as the accessibility of technology. Moving forward within this world requires that we carefully tend to the way content is presented to reach many and encourage the development of a table that not only includes all but bolsters diversity. Through this strategy, we enrich the environment around us and nourish new innovative ideas to move technology forward. Beyond my love for technology, I am a disability advocate, an outdoor enthusiast, a reader, a writer, a dancer, and a singing lover of life. I know that by keeping our lives rounded and our mind entrenched in learning we can move forward in a positive way, bringing out not only the best in ourselves but in those around us. My goals are to be a decisive designer and a valuable team member always striving for not only my best but the best from my team. I enjoy working with others and love when design can be refined within an attainable scope.',
+		highlights:
+			'Tutor/Coach at Kenzie as well as Student Committee Member, Previously taught HTML, CSS and JS, Took on Advanced Course Curriculum',
+		skills: ['Html', 'CSS', 'SASS', 'SCSS', 'JavaScript', 'React', 'Node.js', 'MongoDB'],
+		education: [
+			{
+				title: 'UX Designer',
+				company: 'Kenzie Academy',
+				from: '01/28/2020',
+				to: '07/24/2020',
+			},
+			{
+				title: 'Front End Developer',
+				company: 'Kenzie Academy',
+				from: '07/24/2020',
+				to: '01/22/2021',
+			},
+		],
+		projects: [
+			{
+				type: 0,
+				title: '',
+				text: '',
+				images: false,
+			},
+		],
+		contact: 'mrssilentwar@gmail.com',
+	},
+	{
+		id: 528,
+		name: 'Ryne Tate',
+		image: require('./assets/rynetate-avatar.jpg'),
+		role: 'UX Designer/Front-End Developer',
+		about: '30-something toy geek. Analytical and social.',
+		highlights: 'Managed to graduate from Kenzie Academy during a pandemic. Watched all of Star Wars Rebels.',
+		skills: ['Background in data analysis', 'Comfortable as project manager', 'Growing knowledge in CSS, JS, & React'],
+		education: [
+			{
+				title: 'Bachelor of Arts',
+				company: 'Monmouth College',
+			},
+		],
+		projects: [
+			{
+				type: 1,
+				brief: '',
+				briefImg: '',
+				outline: '',
+				outlineImg: '',
+				process: '',
+				processImg: '',
+				outcome: '',
+				outcomeImg: '',
+				nextSteps: '',
+				nextStepsImg: '',
+				learned: '',
+				learnedImg: '',
+			},
+		],
+		contact: 'rynetate@gmail.com',
+	},
+	{
+		id: 394,
+		name: 'Jordan Burke',
+		image: require('./assets/jordanburke.jpg'),
+		role: 'UX Designer/ Front-End Dev',
+		about:
+			'I am a UX Engineer with a background in graphic design! I enjoy getting to the bottom of difficult problems and creating human centered solutions! ',
+		highlights: '',
+		skills: [
+			'Adobe Illustrator',
+			'Adobe Photoshop',
+			'Adobe XD',
+			'Adobe InDesign',
+			'Sketch',
+			'Figma',
+			'HTML',
+			'CSS',
+			'JavaScript',
+		],
+		education: [
+			{
+				title: 'Art & Design',
+				company: 'Rocky Mountain College of Art & Design',
+			},
+			{
+				title: 'Digital Marketing & Sales',
+				company: 'GreenFig',
+			},
+			{
+				title: 'Development',
+				company: 'General Assembly',
+			},
+			{
+				title: 'UX Designer',
+				company: 'Kenzie Academy',
+				from: '01/28/2020',
+				to: '07/24/2020',
+			},
+			{
+				title: 'Front End Developer',
+				company: 'Kenzie Academy',
+				from: '07/24/2020',
+				to: '01/22/2021',
+			},
+		],
+		projects: [
+			{
+				type: 1,
+				brief: '',
+				briefImg: '',
+				outline: '',
+				outlineImg: '',
+				process: '',
+				processImg: '',
+				outcome: '',
+				outcomeImg: '',
+				nextSteps: '',
+				nextStepsImg: '',
+				learned: '',
+				learnedImg: '',
+			},
+		],
+		contact: 'jordan.burke394@gmail.com',
 	},
 ];
+
+export default userProfiles;
